bf2367d6019e841697342d3b1be2e2bc2c3fe453
mouridev843 <ahmeddameseck7@gmail.com>
2025-10-14 17:17:30 +0000

    added files: package.json, vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "git@github.com:mouridev843/fallyaraam.git",
   server: {
     port: 3000,
     open: true,
